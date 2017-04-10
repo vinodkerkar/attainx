8bbc30a27b24f3af0ebe6b451c086c6621248b2a
Vinod Kerkar <vinodkerkar@gmail.com>
2017-04-10 08:30:07 -0400

    New design first draft

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,1 +1,53 @@
+/*----------------------------/
+	/* CAROUSEL
+	/*---------------------------*/
 
+	if($('.slick-carousel').length > 0) {
+		$('.recent-works.slick-carousel .portfolio-container').slick({
+			dots: true,
+			slidesToShow: 3,
+			cssEase: 'ease-in',
+			prevArrow: '<button type="button" data-role="none" class="btn slick-prev">Previous</button>',
+			nextArrow: '<button type="button" data-role="none" class="btn slick-next">Next</button>',
+			responsive: [
+				{
+					breakpoint: 993,
+					settings: {
+						slidesToShow: 2
+					}
+				},
+				{
+					breakpoint: 481,
+					settings: {
+						slidesToShow: 1
+					}
+				}
+			]
+		});
+
+		$('.testimonial.slick-carousel .testimonial-container').slick({
+			speed: 500,
+			fade: true,
+			prevArrow: '<button type="button" data-role="none" class="btn slick-prev">Previous</button>',
+			nextArrow: '<button type="button" data-role="none" class="btn slick-next">Next</button>',
+		});
+
+		$('#carousel-hero .carousel-inner').slick({
+			speed: 800,
+			dots: true,
+			fade: true,
+			autoplay: true,
+			autoplaySpeed: 2500,
+			prevArrow: '<button type="button" data-role="none" class="carousel-control left slick-prev">Previous</button>',
+			nextArrow: '<button type="button" data-role="none" class="carousel-control right slick-next">Next</button>',
+		});
+	}
+
+
+	/*----------------------------/
+	/* PARALLAX
+	/*---------------------------*/
+
+	if($('.parallax').length > 0) {
+		$('.parallax').stellar();
+	}
