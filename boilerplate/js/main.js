fe2578aeb87652f5f9590b1f25dee2f2eb51f1f4
savroop <singhbal4870@gmail.com>
2017-04-27 19:46:41 -0700

    slick slider hero image and nav bar hover issue fix

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,41 +1,10 @@
-/*----------------------------/
-	/* CAROUSEL
-	/*---------------------------*/
 
-	if($('.slick-carousel').length > 0) {
-		$('.responsive').slick({
-			dots: true,
-			infinite:false,
-			slidesToShow: 3,
-			slidesToScroll: 3,
-			autoplay:true,
-            autoplaySpeed:3000,
-			responsive: [
-				{
-					breakpoint: 561,
-					settings: {
-       					slidesToShow: 3,
-				        slidesToScroll: 3,
-				        infinite: true
-      }
-				},
-				{
-					breakpoint: 560,
-					settings: {
-						slidesToShow: 1,
-						slidesToScroll: 1,
-						infinite: true
-					}
-				}
-			]
-		});
-	}
-
-
-	/*----------------------------/
-	/* PARALLAX
-	/*---------------------------*/
-
-	if($('.parallax').length > 0) {
-		$('.parallax').stellar();
-	}
+$(document).ready(function(){
+$('.responsive').slick({
+        dots: true,
+        infinite: true,
+        autoplay:true,
+        autoplaySpeed:1000,
+        arrows:false
+    });
+});
