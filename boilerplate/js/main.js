40233dc2aaf51f2f2be737f175397683b6354cbc
savroop <singhbal4870@gmail.com>
2017-04-28 12:06:30 -0700

    slick slider for hero image certifications and clients

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,10 +1,32 @@
 
 $(document).ready(function(){
-$('.responsive').slick({
+var slickHeroImage = {
         dots: true,
         infinite: true,
         autoplay:true,
-        autoplaySpeed:1000,
+        autoplaySpeed:3000,
         arrows:false
-    });
+ };
+ var slickCertifications = {
+   dots: true,
+   slidesToShow: 3,
+   slidesToScroll: 3,
+   infinite: true,
+   autoplay:true,
+   autoplaySpeed:3000,
+   arrows:false,
+   responsive: [
+	    {
+	      breakpoint: 559,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+     ]
+ };
+
+ $('.heroImage').slick(slickHeroImage);
+ $('.certifications').slick(slickCertifications);
+ $('.clients').slick(slickCertifications);
 });
